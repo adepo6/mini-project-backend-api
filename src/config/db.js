2740ef9bd7b71b745dd52fbd00536747c5c1c2b7
adepo6 <ladanedesalegn@gmail.com>
2026-02-06 10:15:56 +0400

    finally

--- src/config/db.js
+++ src/config/db.js
@@ -2,12 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        // This connects using the URI from your .env file [cite: 8, 47]
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
-        process.exit(1); // Exit with failure if connection fails [cite: 55]
+        process.exit(1);
     }
 };
 
